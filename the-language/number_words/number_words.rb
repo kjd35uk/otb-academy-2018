4d79e2caff80ec70373b30cb4c620354c42ee672
kirstiedavidson <kjd35uk@yahoo.co.uk>
2018-10-05 13:33:42 +0100

    Tests and code for strings 10 - 19

--- the-language/number_words/number_words.rb
+++ the-language/number_words/number_words.rb
@@ -9,6 +9,16 @@ def number_words(num)
     7 => "seven",
     8 => "eight",
     9 => "nine",
+    10 => "ten",
+    11 => "eleven",
+    12 => "twelve",
+    13 => "thirteen",
+    14 => "fourteen",
+    15 => "fifteen",
+    16 => "sixteen",
+    17 => "seventeen",
+    18 => "eighteen",
+    19 => "nineteen",
   }
   number_strings[num]
 end
--- the-language/number_words/spec/number_words_spec.rb
+++ the-language/number_words/spec/number_words_spec.rb
@@ -11,4 +11,16 @@ RSpec.describe "number_words" do
     expect(number_words (6)).to eq "six"
     expect(number_words (9)).to eq "nine"
   end
+
+  it "returns the string for 10" do
+    expect(number_words (10)).to eq "ten"
+  end
+
+  it "returns the strings for 11 to 19" do
+    expect(number_words (11)).to eq "eleven"
+    expect(number_words (14)).to eq "fourteen"
+    expect(number_words (16)).to eq "sixteen"
+    expect(number_words (19)).to eq "nineteen"
+  end
+
 end
