406473a3687ada9783a811570ab4cbbc6a93cd5f
kirstiedavidson <kjd35uk@yahoo.co.uk>
2018-10-05 13:27:46 +0100

    Add tests and code for nums 2-9

--- the-language/number_words/spec/number_words_spec.rb
+++ the-language/number_words/spec/number_words_spec.rb
@@ -4,4 +4,11 @@ RSpec.describe "number_words" do
   it "returns the string for 1" do
     expect(number_words (1)).to eq "one"
   end
+
+  it "returns the strings for 2 to 9" do
+    expect(number_words (2)).to eq "two"
+    expect(number_words (4)).to eq "four"
+    expect(number_words (6)).to eq "six"
+    expect(number_words (9)).to eq "nine"
+  end
 end
