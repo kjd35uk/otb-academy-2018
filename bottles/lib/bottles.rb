b194303b9df9220a1bb8158f80bcb00ac82520f0
kirstiedavidson <kjd35uk@yahoo.co.uk>
2018-10-11 09:14:16 +0100

    Remove duplication

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -11,24 +11,38 @@ class Bottles
     case number
     when 0
       <<~VERSE
-     No more bottles of beer on the wall, no more bottles of beer.
-     Go to the store and buy some more, 99 bottles of beer on the wall.
-     VERSE
-    when 1
-      <<~VERSE
-      1 bottle of beer on the wall, 1 bottle of beer.
-      Take it down and pass it around, no more bottles of beer on the wall.
-      VERSE
-    when 2
-      <<~VERSE
-      2 bottles of beer on the wall, 2 bottles of beer.
-      Take one down and pass it around, 1 bottle of beer on the wall.
+      #{quantity(number).capitalize} #{container(number)} of beer on the wall, #{quantity(number)} #{container(number)} of beer.
+      Go to the store and buy some more, 99 #{container(99)} of beer on the wall.
       VERSE
     else
       <<~VERSE
-      #{number} bottles of beer on the wall, #{number} bottles of beer.
-      Take one down and pass it around, #{number - 1} bottles of beer on the wall.
+      #{quantity(number)} #{container(number)} of beer on the wall, #{quantity(number)} #{container(number)} of beer.
+      Take #{pronoun(number)} down and pass it around, #{quantity(number - 1)} #{container(number - 1)} of beer on the wall.
       VERSE
+    end
+  end
+
+  def container(number)
+    if number == 1
+      "bottle"
+    else
+      "bottles"
+    end
+  end
+
+  def pronoun(number)
+    if number == 1
+      "it"
+    else
+      "one"
+    end
+  end
+
+  def quantity(number)
+    if number == 0
+      "no more"
+    else
+      number
     end
   end
 end
