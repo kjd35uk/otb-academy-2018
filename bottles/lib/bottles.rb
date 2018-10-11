6e82da7a08aa07595b16c728b57e54d6e7520493
kirstiedavidson <kjd35uk@yahoo.co.uk>
2018-10-11 09:41:54 +0100

    Extract BottleNumber class

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -8,41 +8,50 @@ class Bottles
   end
 
   def verse(number)
+    bottle_number = BottleNumber.new(number)
     case number
     when 0
       <<~VERSE
-      #{quantity(number).capitalize} #{container(number)} of beer on the wall, #{quantity(number)} #{container(number)} of beer.
-      Go to the store and buy some more, 99 #{container(99)} of beer on the wall.
+      #{bottle_number.quantity(number).capitalize} #{bottle_number.container(number)} of beer on the wall, #{bottle_number.quantity(number)} #{bottle_number.container(number)} of beer.
+      Go to the store and buy some more, 99 #{bottle_number.container(99)} of beer on the wall.
       VERSE
     else
       <<~VERSE
-      #{quantity(number)} #{container(number)} of beer on the wall, #{quantity(number)} #{container(number)} of beer.
-      Take #{pronoun(number)} down and pass it around, #{quantity(number - 1)} #{container(number - 1)} of beer on the wall.
+      #{bottle_number.quantity(number)} #{bottle_number.container(number)} of beer on the wall, #{bottle_number.quantity(number)} #{bottle_number.container(number)} of beer.
+      Take #{bottle_number.pronoun(number)} down and pass it around, #{bottle_number.quantity(number - 1)} #{bottle_number.container(number - 1)} of beer on the wall.
       VERSE
     end
   end
 
-  def container(number)
-    if number == 1
-      "bottle"
-    else
-      "bottles"
-    end
-  end
+  class BottleNumber
+  attr_reader :number
 
-  def pronoun(number)
-    if number == 1
-      "it"
-    else
-      "one"
+    def initialize(number)
+      @number = number
     end
-  end
 
-  def quantity(number)
-    if number == 0
-      "no more"
-    else
-      number
+    def container(number)
+      if number == 1
+        "bottle"
+      else
+        "bottles"
+      end
+    end
+
+    def pronoun(number)
+      if number == 1
+        "it"
+      else
+        "one"
+      end
+    end
+
+    def quantity(number)
+      if number == 0
+        "no more"
+      else
+        number
+      end
     end
   end
 end
