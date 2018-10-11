03dc46c7f67df0c81567a9d3409c74dc1285fb8a
kirstiedavidson <kjd35uk@yahoo.co.uk>
2018-10-11 09:41:54 +0100

    Extract BottleNumber class

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -8,41 +8,59 @@ class Bottles
   end
 
   def verse(number)
-    case number
-    when 0
+    bottle_number = BottleNumber.new(number)
+    next_bottle_number = BottleNumber.new(bottle_number.successor(number))
       <<~VERSE
-      #{quantity(number).capitalize} #{container(number)} of beer on the wall, #{quantity(number)} #{container(number)} of beer.
-      Go to the store and buy some more, 99 #{container(99)} of beer on the wall.
+      #{bottle_number.quantity(number).capitalize} #{bottle_number.container(number)} of beer on the wall, #{bottle_number.quantity(number)} #{bottle_number.container(number)} of beer.
+      #{ bottle_number.action(number) }, #{bottle_number.quantity(bottle_number.successor(number))} #{bottle_number.container(bottle_number.successor(number))} of beer on the wall.
       VERSE
-    else
-      <<~VERSE
-      #{quantity(number)} #{container(number)} of beer on the wall, #{quantity(number)} #{container(number)} of beer.
-      Take #{pronoun(number)} down and pass it around, #{quantity(number - 1)} #{container(number - 1)} of beer on the wall.
-      VERSE
-    end
   end
 
-  def container(number)
-    if number == 1
-      "bottle"
-    else
-      "bottles"
-    end
-  end
+  class BottleNumber
+  attr_reader :number
 
-  def pronoun(number)
-    if number == 1
-      "it"
-    else
-      "one"
+    def initialize(number)
+      @number = number
     end
-  end
 
-  def quantity(number)
-    if number == 0
-      "no more"
-    else
-      number
+    def container(number)
+      if number == 1
+        "bottle"
+      else
+        "bottles"
+      end
+    end
+
+    def pronoun(number)
+      if number == 1
+        "it"
+      else
+        "one"
+      end
+    end
+
+    def quantity(number)
+      if number == 0
+        "no more"
+      else
+        number.to_s
+      end
+    end
+
+    def action(number)
+      if number == 0
+        "Go to the store and buy some more"
+      else
+        "Take #{ pronoun(number) } down and pass it around"
+      end
+    end
+
+    def successor(number)
+      if number == 0
+        99
+      else
+        number - 1
+      end
     end
   end
 end
